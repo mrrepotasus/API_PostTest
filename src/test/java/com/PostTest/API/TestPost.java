c1d819908912e1608d79f02d62488c4968c343fe
Ramma Riyantka Putra <ranna203@gmail.com>
2022-08-24 22:37:12 +0700

    Post Test 22

--- src/test/java/com/PostTest/API/TestPost.java
+++ src/test/java/com/PostTest/API/TestPost.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 import io.restassured.http.ContentType;
 
 public class TestPost {
-//	postTest 21
+//	postTest 22
 	@Test
 	public void testPost() {
 		//https://reqres.in/api/users
@@ -22,6 +22,57 @@ public class TestPost {
 		.contentType(ContentType.JSON).accept(ContentType.JSON)
 		.body(request.toJSONString()).when().post("https://mern-backend-8881.herokuapp.com/products")
 		.then().statusCode(200).log().all();
+	}
+	
+	@Test
+	public void testPut() {
+		JSONObject request = new JSONObject();
+		request.put("name","NVIDIA");
+		request.put("category", "VGA");
+		request.put("price", 5000000);
+		System.out.println(request.toJSONString());
+		
+		given()
+		.header("Content-Type","application/json")
+		.contentType(ContentType.JSON)
+		.accept(ContentType.JSON)
+		.body(request.toJSONString())
+		.when()
+		.put("https://mern-backend-8881.herokuapp.com/products")
+		.then()
+		.statusCode(404)
+		.log().all();
+	}
+	
+	@Test
+	public void testPatch() {
+		JSONObject request = new JSONObject();
+		request.put("name","NVIDIA");
+		request.put("category", "VGA");
+		request.put("price", 5000000);
+		System.out.println(request.toJSONString());
+		
+		given()
+		.header("Content-Type","application/json")
+		.contentType(ContentType.JSON)
+		.accept(ContentType.JSON)
+		.body(request.toJSONString())
+		.when()
+		.patch("https://mern-backend-8881.herokuapp.com/products")
+		.then()
+		.statusCode(404)
+		.log().all();
+	}
+	
+	@Test
+	public void testDelete() {
+		
+		when()
+		.delete("https://mern-backend-8881.herokuapp.com/products")
+		.then()
+		.statusCode(404)
+		.log().all();
+		
 	}
 	
 }
